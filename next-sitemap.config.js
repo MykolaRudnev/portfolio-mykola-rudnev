58fab87c7a46885d84184a2ebcc28082d4c0fec9
Mykola Rudnev <rudnevmykola@gmail.com>
2024-08-06 12:47:36 +0200

    changed next-sitemap.config.ts to next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.mrudnev.com',
+    generateRobotsTxt: true, // (opcjonalne) Generuje robots.txt
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7,
+}
